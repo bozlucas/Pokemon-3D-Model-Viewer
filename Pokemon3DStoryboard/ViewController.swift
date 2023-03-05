fd7f762add2a75430ad99eaba57c48797824f7a9
Lucas <lucasprc93@gmail.com>
2023-03-04 17:16:25 -0800

    Initial comit

--- Pokemon3DStoryboard/ViewController.swift
+++ Pokemon3DStoryboard/ViewController.swift
@@ -22,11 +22,8 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
         
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-        
-        // Set the scene to the view
-        sceneView.scene = scene
+        sceneView.autoenablesDefaultLighting = true
+
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -34,6 +31,15 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
+        
+        if let imageToTrack = ARReferenceImage.referenceImages(inGroupNamed: "Pokemon Cards", bundle: Bundle.main) {
+            configuration.detectionImages = imageToTrack
+            configuration.maximumNumberOfTrackedImages = 2
+            
+            
+        }
+        
+
 
         // Run the view's session
         sceneView.session.run(configuration)
@@ -48,27 +54,50 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 
     // MARK: - ARSCNViewDelegate
     
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
     func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
+        
         let node = SCNNode()
-     
+        
+        if let imageAnchor = anchor as? ARImageAnchor{
+            
+            
+            
+            let plane = SCNPlane(width: imageAnchor.referenceImage.physicalSize.width, height: imageAnchor.referenceImage.physicalSize.height)
+            
+            plane.firstMaterial?.diffuse.contents = UIColor(white: 1.0, alpha: 0.5)
+            
+            let planeNode = SCNNode(geometry: plane)
+            
+            planeNode.eulerAngles.x = -.pi/2
+            
+            node.addChildNode(planeNode)
+            
+            if imageAnchor.referenceImage.name == "gardevoir-card"{
+                if let pokemonScene = SCNScene(named: "art.scnassets/gardevoir-scene-new.scn"){
+                    if let pokemonNode = pokemonScene.rootNode.childNodes.first {
+                        
+                        pokemonNode.eulerAngles.x = .pi
+                        planeNode.addChildNode(pokemonNode)
+                    }
+                }
+            }
+            
+            if imageAnchor.referenceImage.name == "chewtle-card"{
+                if let pokemonScene = SCNScene(named: "art.scnassets/chewtle-scene.scn"){
+                    if let pokemonNode = pokemonScene.rootNode.childNodes.first {
+                        
+                        pokemonNode.eulerAngles.x = .pi
+                        planeNode.addChildNode(pokemonNode)
+                    }
+                }
+            }
+            
+            
+            
+            
+        }
+        
         return node
     }
-*/
-    
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
-        
-    }
-    
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
-        
-    }
-    
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
-        
-    }
+
 }
---
+++ README.md
@@ -0,0 +1,31 @@
+#  Pokemon 3D Model Viewer
+
+
+## Description
+This iOS app allows users to view a 3D model of a Pokemon based on its Pokemon TCG card using ARKit, SceneKit, and UIKit technologies. The app uses the camera to detect the TCG card and displays a 3D model of the Pokemon on top of it.
+
+## Features
+- AR detection of Pokemon TCG cards
+- 3D model display of Pokemon based on the card detected
+- Users can interact with the 3D model by rotating the card and changing the angle
+
+## Technologies Used
+
+- ARKit for Pokemon TCG card detection and placement of the 3D model
+- SceneKit for creating the 3D model and rendering it
+- UIKit for creating the app's user interface and controls
+
+# Installation
+- Clone the project from GitHub: git clone https://github.com/bozlucas/Pokemon-3D-Model-Viewer.git
+- Open the project in Xcode
+- Build and run the app on your device or simulator
+
+# Usage
+
+- Launch the app on your iOS device
+- Point the camera at a Pokemon TCG card
+- The app will detect the card and display a 3D model of the Pokemon on top of it
+- Rotate the card without blocking its image to interact with the 3D model (rotate, zoom, etc.)
+
+**Compatible TCG cards: Gardevoir 061/198 and Chewtle 026/072**
+
